e5cc7b2a3a50ca21830d9e24ca06358ad2ba8f8a
hsnee <2061022a@student.gla.ac.uk>
2015-11-01 18:45:51 +0000

    -

--- coordinateshift.py
+++ coordinateshift.py
@@ -1,22 +1,35 @@
-def get_coordsdiff(T1=967966720):
+def get_coordsdiff(x,T1=967966720):
 	from astropy.coordinates import get_sun
 	import astropy.time as Time
-	
+	import numpy as np
+	import lal
+
+
 	T1 =  967966720
 	t = Time.Time(T1,format='gps')
 	t.coords = get_sun(t)
 	coords = get_sun(t)
-	T2 = T1 + 2400
+	T2 = T1 + x
 	t2 = Time.Time(T2,format='gps')
 	coords2 = get_sun(t2)
-	dec = coords.dec.hour
-	dec2 = coords2.dec.hour
-	
-	# Difference of declination after 40 minutes (duration of 1 hdf5 file)
-	diff_dec = dec2-dec
-	print('Difference in declination between T = 967966720 GPS time and 40 minutes later',
-	'is', diff_dec)
-	# Doing the same for right ascension:
-	diff_ra = coords2.ra.hour - coords.ra.hour
-	print('Difference in right ascention between T = 967966720 GPS time and 40 minutes later',
-	'is', diff_ra)
+	dec = coords.dec.hour * np.pi/12
+	ra = coords.ra.hour *np.pi/12
+	ra2 = coords.ra.hour * np.pi/12
+	dec2 = coords2.dec.hour * np.pi/12
+
+	detMap = {'H1': lal.LALDetectorIndexLHODIFF, 'L1':
+	lal.LALDetectorIndexLLODIFF}
+	detH1 = lal.CachedDetectors[detMap['H1']]
+	detL1 = lal.CachedDetectors[detMap['L1']]
+	tgps = lal.LIGOTimeGPS(T1, 0)
+	tgps2 = lal.LIGOTimeGPS(T2, 0)
+
+	coords = get_sun(Time.Time(967966720.0,format='gps'))
+	ra  = coords.ra.hour  * np.pi/12
+	dec = coords.dec.hour * np.pi/12
+
+
+	tdelay1 = lal.ArrivalTimeDiff(detH1.location, detL1.location, ra, dec, tgps)
+	tdelay2 = lal.ArrivalTimeDiff(detH1.location, detL1.location, ra2, dec2, tgps2)
+	diff_dec = tdelay2-tdelay1
+
