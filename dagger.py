ce44842d650d2d51e59c9a6405b9f9ac1998482f
hsnee <2061022a@student.gla.ac.uk>
2015-11-27 14:07:38 +0000

    -

--- dagger.py
+++ dagger.py
@@ -2,13 +2,13 @@
 import numpy as np
 
 # first read in
-timearray=np.array(np.loadtxt('intersect.txt',dtype='f8'))
+timearray=np.array(np.loadtxt('intersect6.txt',dtype='f8'))
 StartTimes = timearray[:,0]
 EndTimes   = timearray[:,1]
 
 # now to write the .dag file
 file = open("mainjob.dag", "w")
 for i in range(len(StartTimes)):
-	file.write('JOB '+str(i+1)+' job.sub\n')
-	file.write('VARS '+str(i+1)+' starttime='+str(StartTimes[i])+', endtime='+str(EndTimes[i])+', outputfile='+str(int(StartTimes[i]))+'\n')
+	file.write('JOB '+str(i+1)+' testjob.sub\n')
+	file.write('VARS '+str(i+1)+' starttime='+str(int(StartTimes[i]))+' endtime='+str(int(EndTimes[i]))+'\n')
 file.close()
