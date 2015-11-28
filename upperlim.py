ca1d35e8e7ac7026128ef4ffbde6cdc5da0a3df6
hsnee <2061022a@student.gla.ac.uk>
2015-11-28 00:58:44 +0000

    -

--- upperlim.py
+++ upperlim.py
@@ -1,5 +1,6 @@
 # Getting an upper limit on solarGWs
-
-
-N = duration/Xspacing
-h = sqrt(p/N)
+def upperlim(p,duration):
+	Xspacing = 2.44140625E-4
+	N = duration/Xspacing
+	h = sqrt(p/N)
+	print h
