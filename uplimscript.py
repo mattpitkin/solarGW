ae7b62f8d8885adf31fb693d762623fb3f6f2bcb
hsnee <2061022a@student.gla.ac.uk>
2015-11-29 02:40:50 +0000

    -

--- uplimscript.py
+++ uplimscript.py
@@ -1,22 +1,28 @@
 import numpy as np
 import os, h5py
-from upperlim import uplim
+from upperlim import uppperlim
 
 # Read in the intersect6.txt file
 timearray  = np.array(np.loadtxt('intersect6.txt',dtype='f8'))
 StartTimes = timearray[:,0]
 EndTimes   = timearray[:,1]
-duration = 0
+duration = 1
 strain = 0
 for i in range(len(StartTimes)):
-	h5path =  'scaratch/spxha/'+str(StartTimes[i])+'_'+str(EndTimes[i])
+	h5path =  'scratch/spxha/'+str(StartTimes[i])+'_'+str(EndTimes[i])
 	if os.path.exists(h5path):
 		durationi = EndTimes[i] - StartTimes[i]
-		duration = duration + durationi
-		f = h5py.File(h5path+str(StartTimes[i])+'.hdf5','r')
-		for j in range(len(f['Signal'])):
-			strainj = f['Signal/Signal'+str(j)].value
-			if j==0:
-				strain = strain + strainj
-result = uplim(strain,duration)
-print result
+		if (durationi>180):
+			duration = duration + durationi - 150
+			f = h5py.File(h5path+str(StartTimes[i])+'.hdf5','r')
+			for j in range(5,len(f['Signal'])):
+				strainj = f['Signal/Strail'+str(j)].value
+				if j==0:
+					strain = strain + strainj
+				else:
+					pass
+		else:
+			pass
+	else:
+		pass
+result = upperlim(strain,duration)
