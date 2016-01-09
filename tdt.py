8f6225f517f2459afb699f1d05609c0a452a28d8
hsnee <2061022a@student.gla.ac.uk>
2016-01-09 16:17:18 +0000

    -

--- tdt.py
+++ tdt.py
@@ -20,16 +20,16 @@ coords = get_sun(t)
 dec = coords.dec.hour * np.pi/12
 ra  = coords.ra.hour  * np.pi/12
 # again looping for same reason as above
-for i in range(100000):
-	tgps[i] = lal.LIGOTimeGPS(T1[i], 0)
-	tdelay[i] = lal.ArrivalTimeDiff(detH1.location, detL1.location, ra[i], dec[i], tgps[i])
-j_s= np.linspace(1,162,162)*600
-ddt = [[0] for _ in range(162)]
-a = np.linspace(1,160,160)*600
-a = a.astype(int)
-for j in a:
-	ddt[j/600]=tdelay[j+600]-tdelay[j]
-T_s = T0 + j_s
+# for i in range(100000):
+# 	tgps[i] = lal.LIGOTimeGPS(T1[i], 0)
+# 	tdelay[i] = lal.ArrivalTimeDiff(detH1.location, detL1.location, ra[i], dec[i], tgps[i])
+# j_s= np.linspace(1,162,162)*600
+# ddt = [[0] for _ in range(162)]
+# a = np.linspace(1,160,160)*600
+# a = a.astype(int)
+# for j in a:
+# 	ddt[j/600]=tdelay[j+600]-tdelay[j]
+# T_s = T0 + j_s
 
 j_s2= np.linspace(1,320,320)*300
 ddt2 = [[0] for _ in range(320)]
@@ -55,12 +55,12 @@ for j in a4:
 	ddt4[j/30]=tdelay[j+30]-tdelay[j]
 T_s4 = T0 + j_s4
 
-plt.plot(T_s[1:160], ddt[1:160],color="blue",label='10 minute segments')
+# plt.plot(T_s[1:160], ddt[1:160],color="blue",label='10 minute segments')
 plt.plot(T_s2[1:315], ddt2[1:315],color="red",label='5 minute segments')
 plt.plot(T_s3[1:1500], ddt3[1:1500],color="green",label='1 minute segments')
 plt.plot(T_s4[1:3100], ddt4[1:3100],color="black",label='1/2 minute segments')
 plt.xlabel('GPS Time (s)')
 plt.ylabel('Change in time delay')
-plt.title('Change in time delay for different time segments')
+# plt.title('Change in time delay for different time segments')
 plt.legend(fancybox=True)
 plt.show()
