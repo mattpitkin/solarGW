20b7ee2d98ded00e96936ea453109e6ff5a68e8a
hsnee <2061022a@student.gla.ac.uk>
2016-02-23 18:25:58 +0000

    -

--- solarbayes/lookuptab.py
+++ solarbayes/lookuptab.py
@@ -1,13 +1,9 @@
 #------- Packages ---------#
 from __future__ import division
 import numpy as np
-import astropy
-import gwpy
-import h5py
-import lal
+import astropy, gwpy, h5py, lal, astropy
 from astropy.coordinates import get_sun
 import astropy.time as Time
-import astropy
 from gwpy.timeseries import TimeSeries
 from antres import antenna_response as ant_res
 
@@ -17,9 +13,10 @@ f = h5py.File("/home/spxha/","w")
 #------- Define/workout variables -------#
 #for every 10 minutes, I was to create (sub-?)arrays of ra, dec,
 starttime=931219808
+endtime = 971614889
+dtime = endtime - starttime
 
-#endime =
-numseg = np.int((endtime-starttime)/600)
+numseg = np.int((dtime)/600)
 segs = np.linspace(1,numseg,numseg)*600
 segs = segs + starttime - 600
 ra,dec,fp,fc = [[0 for _ in range(numseg)] for _ in range(4)]
@@ -30,9 +27,12 @@ for i in range(numseg):
 	dec[i] = coords.dec.hour*np.pi/12
 
 #------ Creating lookup table -----#
-# psi = Jeffrey's prior?
 # det is practically either H1 or L1
-fp, fc = ant_res(segs, ra, dec, psi, det):
+psi_array = np.linspace(0,np.pi,10)
+fp,fc = [[[[0] for _ in range(numseg)] for _ in range(10)] for _ in range(2)]
+for i in range(len(psi_array)):
+	for j in range(numseg):
+		fp[i][j], fc[i][j] = ant_res(segs[j], ra[j], dec[j], psi_array[i], H1):
 
 
 #------- Write to hdf5 --------#
@@ -40,9 +40,12 @@ time_dset = f.create_dataset(segs,'time')
 time_dset.attrs['Definition'] = "GPS Time"
 ra_dset = f.create_dataset(ra,'ra')
 ra_dset.attrs['Definition'] = "Right Ascension"
+ra_dset = f.create_dataset(psi_array,'psi')
+ra_dset.attrs['Definition'] = "Polarisation Angle"
 dec_dset = f.create_dataset(dec,'dec')
 dec_dset.attrs['Definition'] = "Declination"
-fp_dset = f.create_dataset(fp,'fp')
-fp_dset.attrs['Definition'] = "Antenna Response - Plus Polarised"
-fc_dset = f.create_dataset(fc,'fc')
-fc_dset.attrs["Defintion"]="Antenna Response - Cross Polarised"
+for i in range(len(psi_array)):
+	fp_dset = f.create_dataset(fp[i],'fp')
+	fp_dset.attrs['Definition'] = "Antenna Response - Plus Polarised"+str(i)
+	fc_dset = f.create_dataset(fc[i],'fc')
+	fc_dset.attrs["Defintion"]="Antenna Response - Cross Polarised"+str(i)
--- solarbayes/p(h0_psi_I).py
+++ solarbayes/p(h0_psi_I).py
@@ -1,41 +1,43 @@
 #------- Packages ---------#
 from __future__ import division
 import numpy as np
-import astropy
-import gwpy
-import h5py
-import lal
+import astropy, gwpy, h5py, lal, astropy
 from astropy.coordinates import get_sun
 import astropy.time as Time
-import astropy
 from gwpy.timeseries import TimeSeries
 from antres import antenna_response as ant_res
-from scipy.integrate import quad, dblquad
 
-# we already know fp, fc and psi(?,integration) by now, and obviously h(L) and h(Y)
+# from scipy.integrate import quad, dblquad
 
 # need to define standard deviations: sig_x, sig_y, sig_A
 
+Xspacing = 2.44140625E-4
 # for i in range(time/Xspacing):
 
-V =  h0**2 * sig_Y * ((fc_X/ sig_X**2) + (fc_Y/sig_Y**2))
+# data vector (for detectors X and Y)
+d = np.array([dX, dY])
 
-N1 = - sig_A**2 * (2 * d_X * d_Y * (fp_X * p_Y + fc_X * fc_Y) * \
-h0**2 + d_Y * d_X**2 *h0 * (fp_X**2 + fc_X**2) + sig_A**2 * sig_X**2) )
+# design matrix
+M = h0*np.array([[FpX, FpY], [FcX, FcY]])
 
-N2 = 2*(-2*h0**4 * fc_X * fc_Y * fp_X * fp_Y + \
-((h0**2 fc_X**2*(fp_X**2*h0**2+sig_X**2*sig_A**2)) + \
-(h0**2 fc_Y**2*(fp_Y**2*h0**2+sig_Y**2*sig_A**2))) + \
-sig_A**2 * ((fc_X**2*h0**2*sig_X**2) + (fc_Y**2*h0**2*sig_Y**2) + \
-sig_A**2 *sig_X**2))
+# the inverse data covariance matrix and the determinant of the covariance matrix
+C = np.array([[sigmaX**2, 0.], [0., sigmaY**2]])
+invC = np.array([[(1./sigmaX**2), 0.], [0., (1/sigmaY**2)]])
+detC = sigmaX**2 * sigmaY**2
 
-p = (1/ (2 * np.pi * sig_A**2 * sig_x[i] * np.sqrt(sig_A**2 + V) ))* np.exp(N1/N2)
+# inverse of the amplitude prior covariance matrix
+invSigma0 = np.array([[(1./sigmaA**2) 0.], [0., (1./sigmaA**2)]])
+detSigma0 = sigmaA**4
 
-#def p(Ac,Ap):
-#	return  (1/2*np.pi*sig_x*sig_y)*exp()
+# inverse of Sigma
+invSigma = np.dot(M.T, np.dot(invC, M)) + invSigma0
+Sigma = np.linalg.inv(invSigma)
+detSigma = np.linalg.det(Sigma)
 
+# chi
+chi = np.dot(Sigma, np.dot(M.T, np.dot(invC, d)))
 
-#ans, err = dblquad(integrand,-np.inf,+np.inf,
-#lambda x: -np.inf,
-#lambda x: +np.inf,
-#args=(,,,))
+# log likelihood
+p = 0.5*np.log(detSigma) - 0.5*log(16.*np.pi**4*detSigma0*detC) -
+    0.5*(np.vdot(d.T, np.dot(invC, d)) + np.vdot(chi.T, np.dot(invSigma, chi)))
+
