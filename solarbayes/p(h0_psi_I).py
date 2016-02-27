9736956621947dff9046e5ca2ed56e53faf33ff9
hsnee <2061022a@student.gla.ac.uk>
2016-02-27 15:56:16 +0000

    -

--- solarbayes/p(h0_psi_I).py
+++ solarbayes/p(h0_psi_I).py
@@ -7,30 +7,25 @@ import astropy.time as Time
 from gwpy.timeseries import TimeSeries
 from antres import antenna_response as ant_res
 
-
-#-------- First importing some stuff from earlier work --------#
-starttime=931219808
-endtime = 971614889
-
+#-------- Filtering and timeshifting detectors --------#
+starttime = 931219808
+endtime   = 971614889
 gpsStartH = starttime
 durationH = endtime - starttime
-gpsEndH = endtime
-
+gpsEndH   = endtime
 gpsStartL = gpsStartH
 durationL = durationH
-gpsEndL = gpsEndH
-
+gpsEndL   = gpsEndH
 #---------------------------
 # Applying a bandpass filter
 #---------------------------
 ord = 4
-Wn = [100.0/2918.0,300.0/2918.0]
+Wn = [100.0/2918.0,150.0/2918.0]
 type = 'bandpass'
 bL,aL = butter(ord,Wn, btype=type)
 bH,aH = butter(ord,Wn, btype=type)
 strainL = filtfilt(bL,aL,strainL)
 strainH = filtfilt(bH,aH,strainH)
-
 #---------------------------
 # Create a time vector
 #---------------------------
@@ -42,18 +37,15 @@ timel=timeL
 #-----------------------------------------------
 detMap = {'H1': lal.LALDetectorIndexLHODIFF, 'L1':
 lal.LALDetectorIndexLLODIFF}
-
 #-------------------------------------
 # Get detector structure for H1 and L1
 #-------------------------------------
 detH1 = lal.CachedDetectors[detMap['H1']]
 detL1 = lal.CachedDetectors[detMap['L1']]
-
-#--------------------------------------------------------------
-# Set a GPS time (this is just a random value for this example)
-#--------------------------------------------------------------
+#---------------
+# Set a GPS time
+#---------------
 tgps = lal.LIGOTimeGPS(gpsStartH, 0)
-
 #---------------------------------------------------------
 # Get right ascension and declination of source in radians
 #---------------------------------------------------------
@@ -76,10 +68,10 @@ tdelay = np.append(tdelay,b)
 
 timeL = timeL - tdelay
 
-# H1 and L1 are now in sync.
+# H1 and L1 are now in sync and filtered between 100 and 150 Hz.
 
-#------- Preparing for fc and fp ------#
-numseg = np.int((dtime)/600)
+#------- Defining some stuff for p ------#
+numseg = int((durationH)/600)
 segs = np.linspace(1,numseg,numseg)*600
 segs = segs + starttime - 600
 ra,dec,fp,fc = [[0 for _ in range(numseg)] for _ in range(4)]
@@ -92,33 +84,41 @@ psi_array = np.linspace(0,np.pi,10)
 
 # X is H1 and Y is L1
 
-sigmaX = np.std(StrainH1) #just std of strainH1
-sigmaY = np.std(StrainL1) # std of strainL!
+sigmaX = np.std(strainH) # just std of strainH1
+sigmaY = np.std(strainL) # std of strainL!
 sigmaA = 10.0
-h0 = 3*sigmaA
-
-Xspacing = 2.44140625E-4
-
-###########
-# Next I want to get
-
-# for i in range(time/Xspacing):
-# use function directly for fc and fp
-
-# for psi in psi_array = np.linspace(0,np.pi,10)
-
-# Finding probability distribution
-d = np.array([dX, dY])
-M = h0*np.array([[FpX, FpY], [FcX, FcY]])
-C = np.array([[sigmaX**2, 0.], [0., sigmaY**2]])
-invC = np.array([[(1./sigmaX**2), 0.], [0., (1/sigmaY**2)]])
-detC = sigmaX**2 * sigmaY**2
+h0 = np.lin(0,3*sigmaA,10)
 invSigma0 = np.array([[(1./sigmaA**2) 0.], [0., (1./sigmaA**2)]])
 detSigma0 = sigmaA**4
-invSigma = np.dot(M.T, np.dot(invC, M)) + invSigma0
-Sigma = np.linalg.inv(invSigma)
-detSigma = np.linalg.det(Sigma)
-chi = np.dot(Sigma, np.dot(M.T, np.dot(invC, d)))
+dX = strainH
+dY = strainY
+
+###########
+# Finding probability distribution
+# for psi in psi_array
+d, invC, detC, invSigma, detSigma, chi = [[0 for _ in range(int(durationH/Xspacing))] for _ in range(6)]
+for i in range(int(durationH/Xspacing)):
+	psi = np.pi/2.0
+	FpX, FcX = ant_res(gpsTime[i], ra[i], dec[i], psi, 'H1')
+	FpY, FcY = ant_res(gpsTime[i], ra[i], dec[i], psi, 'L1')
+	d[i] = np.array([dX[i], dY][i])
+	M = h0*np.array([[FpX, FpY], [FcX, FcY]])
+	C = np.array([[sigmaX**2, 0.], [0., sigmaY**2]])
+	invC[i] = np.array([[(1./sigmaX**2), 0.], [0., (1/sigmaY**2)]])
+	detC[i] = sigmaX**2 * sigmaY**2
+	invSigma[i] = np.dot(M.T, np.dot(invC[i], M)) + invSigma0
+	Sigma = np.linalg.inv(invSigma[i])
+	detSigma[i] = np.linalg.det(Sigma)
+	chi[i] = np.dot(Sigma, np.dot(M.T, np.dot(invC, d)))
 p = 0.5*np.log(detSigma) - 0.5*log(16.*np.pi**4*detSigma0*detC) -
     0.5*(np.vdot(d.T, np.dot(invC, d)) + np.vdot(chi.T, np.dot(invSigma, chi)))
 
+#------ plot the probability distribution
+fname = 'probdist.pdf'
+with PdfPages(fname) as pdf:
+	fig1 = plt.figure()
+	plt.plot(h0,p)
+	plt.title('Probability Distribution')
+	pdf.savefig(fig1)
+	plt.close()
+print 'Plot saved as', fname, 'Did we find GWs?!'
