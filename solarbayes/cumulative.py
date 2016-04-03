89498a115a00fa8026cf689fe7ddd130b9dc0a3e
hsnee <2061022a@student.gla.ac.uk>
2016-04-03 14:37:53 +0300

    made some comments

---
+++ solarbayes/cumulative.py
@@ -0,0 +1,60 @@
+# code to add up the probability for a month or a week, prints the duration, and plots the cumulative probability to infer the upper limit with x% confidence. Prints the upper limit at 95% confidence.
+def plotdist(starttime,endtime):
+	# starttime and endtime should be in format 'week'+str(i) or 'month'+str(i) only.
+	# In the near future it will take 'all' as starttime and endtime to plot the entire data.
+	import numpy as np
+	from matplotlib.backends.backend_pdf import PdfPages
+	import matplotlib.pyplot as plt
+	import os
+	fname =	'probdist_cumulative.pdf'
+	fname1 = str(starttime)
+	fname2 = str(endtime)
+	timearray = np.array(np.loadtxt('/home/spxha/solarGW/intersect_old.txt',dtype='f8'))
+	StartTimes = timearray[:,0]
+	EndTimes   = timearray[:,1]
+	if starttime=='all' and endtime=='all':
+		starttime = 931076896.0
+		endtime   = 971614889.0
+	duration = 0
+	newStartTimes = StartTimes[(np.abs(StartTimes-starttime)).argmin():(np.abs(EndTimes-endtime)).argmin()]
+	newEndTimes   =   EndTimes[(np.abs(StartTimes-starttime)).argmin():(np.abs(EndTimes-endtime)).argmin()]
+	p_array,h0_array = [[[0 for _ in range(30)] for _ in range(len(newStartTimes))] for _ in range(2)]
+	j = 0
+	for i in range(len(newStartTimes)):
+		pathi = 'p'+str(int(newStartTimes[i]))+'.txt'
+		if os.path.exists(pathi)==True:
+			j +=1
+			print i
+			p_array[i]  = np.array(np.loadtxt('p'+str(int(newStartTimes[i]))+'.txt',dtype='float'))
+			h0_array[i] = np.array(np.loadtxt('h0'+str(int(newStartTimes[i]))+'.txt',dtype='float'))
+			duration += newEndTimes[i]-newStartTimes[i] - 150
+		else:
+			pass
+	print j
+	p_sum_array = [0.0 for _ in range(30)]
+	p_sum_array = np.array(p_sum_array)
+	h0_array = np.array(h0_array)
+	print p_array[1]
+	p_array = np.array(p_array)
+	for i in range(len(p_array)-1):
+		p_sum_array += p_array[i+1]
+	h0_mean_array = h0_array.mean(0)
+	p = np.exp(p_sum_array-np.max(p_sum_array))
+	print h0_mean_array,p
+
+	# Making a cumulative plot to get the 95% value.
+	prob = [0 for _ in range(len(p))]
+	for i in range(len(h0_array)):
+		for j in range(i+1):
+			d = [0 for _ in range(i+1)]
+			d[j] = p[i-j]
+		prob[i] = np.sum(d)
+	# plot the distribution
+	with PdfPages(fname) as pdf:
+		fig1 = plt.figure()
+		plt.plot(h0_mean_array,p,'+')
+		plt.title('Probability Distribution for GPS'+str(starttime)+'_'+str(endtime))
+		plt.xlabel('h0')
+		plt.ylabel('Cumulative probability')
+		pdf.savefig(fig1)
+		plt.close()
